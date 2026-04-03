2346f970b1fff0fcf1b67bf31d90ffe76447f6fe
GabrieleBattaglia <iz4apu@libero.it>
2026-04-03 14:04:52 +0200

    Aggiornata versione a 7.0.0

--- orologic_modules/version.py
+++ orologic_modules/version.py
@@ -2,6 +2,6 @@ import datetime
 
 #QC
 BIRTH_DATE = datetime.datetime(2025, 2, 14, 10, 16)
-VERSION = "6.5.1"
-RELEASE_DATE = datetime.datetime(2026, 4, 3, 8, 13)
+VERSION = "7.0.0"
+RELEASE_DATE = datetime.datetime(2026, 4, 3, 14, 3)
 PROGRAMMER = "Gabriele Battaglia & AIs"
