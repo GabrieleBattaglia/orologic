0c89a8789f65b74784449ab7e97f38ef5440ba7b
GabrieleBattaglia <iz4apu@libero.it>
2026-04-23 23:52:15 +0200

    Bump version to 7.3.4 and fix updater bug

--- orologic_modules/version.py
+++ orologic_modules/version.py
@@ -2,6 +2,6 @@ import datetime
 
 # QC
 BIRTH_DATE = datetime.datetime(2025, 2, 14, 10, 16)
-VERSION = "7.3.3"
-RELEASE_DATE = datetime.datetime(2026, 4, 23, 10, 30)
+VERSION = "7.3.4"
+RELEASE_DATE = datetime.datetime(2026, 4, 24, 0, 0)
 PROGRAMMER = "Gabriele Battaglia & AIs"
