e75ce5bdc4b1803de8dd30d7ee22239d0afd16ed
GabrieleBattaglia <iz4apu@libero.it>
2026-07-27 14:50:36 +0200

    Rilascio v8.9.3: risolta Issue #32 (localizzazione completa di Orolichess e aggiornamento cataloghi i18n)

--- orologic_modules/version.py
+++ orologic_modules/version.py
@@ -2,6 +2,6 @@ import datetime
 
 # QC
 BIRTH_DATE = datetime.datetime(2025, 2, 14, 10, 16)
-VERSION = "8.9.2"
-RELEASE_DATE = datetime.datetime(2026, 7, 27, 13, 10)
+VERSION = "8.9.3"
+RELEASE_DATE = datetime.datetime(2026, 7, 27, 14, 48)
 PROGRAMMER = "Gabriele Battaglia & Stella"
