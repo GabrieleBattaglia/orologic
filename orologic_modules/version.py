6cd4db84635186df308a56b1c1da4b6944d82c7d
GabrieleBattaglia <iz4apu@libero.it>
2026-07-28 09:06:14 +0200

    Rilascio v8.9.4: risolto bug avvio polipo dopo aggiornamento

--- orologic_modules/version.py
+++ orologic_modules/version.py
@@ -2,6 +2,6 @@ import datetime
 
 # QC
 BIRTH_DATE = datetime.datetime(2025, 2, 14, 10, 16)
-VERSION = "8.9.3"
-RELEASE_DATE = datetime.datetime(2026, 7, 27, 14, 48)
+VERSION = "8.9.4"
+RELEASE_DATE = datetime.datetime(2026, 7, 28, 9, 5)
 PROGRAMMER = "Gabriele Battaglia & Stella"
