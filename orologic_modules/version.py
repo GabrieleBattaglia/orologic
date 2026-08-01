d9ce9249bde6865b1d307b6540a569404142c5db
GabrieleBattaglia <iz4apu@libero.it>
2026-08-01 11:10:57 +0200

    v8.9.8: Aggiunta sezione 1.5 al manuale sulla scacchiera testuale ASCII per screen reader e riga Braille

--- orologic_modules/version.py
+++ orologic_modules/version.py
@@ -2,6 +2,7 @@ import datetime
 
 # QC
 BIRTH_DATE = datetime.datetime(2025, 2, 14, 10, 16)
-VERSION = "8.9.7"
-RELEASE_DATE = datetime.datetime(2026, 7, 31, 7, 15)
+VERSION = "8.9.8"
+RELEASE_DATE = datetime.datetime(2026, 8, 1, 11, 0)
 PROGRAMMER = "Gabriele Battaglia & Stella"
+
